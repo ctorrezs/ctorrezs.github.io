4e883c77ed3f19c52029a45a0d0484ee09f7d579
ctorrezs <ceduardot@icloud.com>
2025-10-26 19:45:02 -0500

    Add files via upload

---
+++ list/code.js
@@ -0,0 +1,13 @@
+var items = ["Computer", "Iphone", "Backpack", "Wallet", "Car", "Ipad", "Glasses", "Slippers", "Charger", "Tennis Racket" ];
+
+document.write("<ul>")
+for(var i = 0; i < items.length; i++ )
+
+{
+    document.write("<li>" + items[i] + "</li>");
+}
+document.write("</ul>");
+
+
+
+
